3a4723bf8ea405750ae58af89278e7511e1d77ab
Abraz-babs <info@glisteninternationalacademy.com>
2026-02-18 12:32:17 +0100

    Fix: Applied missing API update for Full Proposal Generation and bumped version to v2.1

--- src/sections/ProposalAssistant.tsx
+++ src/sections/ProposalAssistant.tsx
@@ -113,7 +113,7 @@ export const ProposalAssistant: React.FC<ProposalAssistantProps> = ({
             disabled={!selectedGrant}
             icon={<Sparkles className="w-4 h-4" />}
           >
-            Generate Full Proposal (v2.0)
+            Generate Full Proposal (v2.1)
           </NeonButton>
         </div>
 
